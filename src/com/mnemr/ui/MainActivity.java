171c5db25fde4b8d16799f659c236b9ff8b9e1cb
Hamid <barzali@andlabs.de>
2011-05-07 18:17:39 +0200

    adapter added :)

--- src/com/mnemr/ui/MainActivity.java
+++ src/com/mnemr/ui/MainActivity.java
@@ -1,29 +1,61 @@
 package com.mnemr.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        
-        ImageButton addbtn = (ImageButton) findViewById(R.id.addmemo_id);
-        //addbtn.setBackgroundResource(android.R.drawable.ic_input_add)
-        ImageButton mnmrListbtn = (ImageButton) findViewById(R.id.mnemrlist_id);
-        ImageButton cardsbtn = (ImageButton) findViewById(R.id.mnemrcards_id);
-        
-        
-        
-        
-        
-        
-        
-        
-    }
+public class MainActivity extends Activity implements OnTouchListener {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		ImageButton addbtn = (ImageButton) findViewById(R.id.addmemo_id);
+
+		ImageButton mnmrListbtn = (ImageButton) findViewById(R.id.mnemrlist_id);
+
+		ImageButton cardsbtn = (ImageButton) findViewById(R.id.mnemrcards_id);
+
+		addbtn.setOnTouchListener(this);
+		mnmrListbtn.setOnTouchListener(this);
+		cardsbtn.setOnTouchListener(this);
+
+	}
+
+	public boolean onTouch(View v, MotionEvent event) {
+
+		if (event.getAction() == MotionEvent.ACTION_UP) {
+
+			if (v instanceof ImageButton) {
+				ImageButton imButton = (ImageButton) v;
+
+				if (imButton.getId() == R.id.addmemo_id) {
+					Toast.makeText(MainActivity.this, "ad mnemo !",
+							Toast.LENGTH_SHORT).show();
+				}
+				if (imButton.getId() == R.id.mnemrlist_id) {
+//					Toast.makeText(MainActivity.this, "list of mnemos ;)",
+//							Toast.LENGTH_SHORT).show();
+					
+					Intent listIntent = new Intent(MainActivity.this,MnemListActivity.class);
+					startActivity(listIntent);
+					
+					
+				}
+				if (imButton.getId() == R.id.mnemrcards_id) {
+					Toast.makeText(MainActivity.this, "flash cards :) !",
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+
+		}
+
+		return false;
+	}
 }
