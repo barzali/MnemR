e42101a5bb7ca9a01f44cc554b79a31277a02400
Hamid <barzali@andlabs.de>
2011-05-10 10:57:35 +0200

    many changes;)

--- src/com/mnemr/ui/MnemListActivity.java
+++ src/com/mnemr/ui/MnemListActivity.java
@@ -3,14 +3,12 @@
  */
 package com.mnemr.ui;
 
-import com.mnemr.controller.MnemoListAdapter;
-import com.mnemr.provider.Mnem;
-
 import android.app.ExpandableListActivity;
+import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -20,6 +18,9 @@ import android.widget.AbsListView;
 import android.widget.CursorTreeAdapter;
 import android.widget.ExpandableListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mnemr.provider.Mnem;
 
 /**
  * @author Hamid
@@ -27,64 +28,75 @@ import android.widget.TextView;
  */
 public class MnemListActivity extends ExpandableListActivity {
 
-	 private ExpandableListAdapter adapter;
+
+	private ExpandableListAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		adapter=new CursorTreeAdapter(getContentResolver().query(Mnem.CONTENT_URI, Mnem.PROJECTION, null, null, null), this) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
 
-			@Override
-			protected View newGroupView(Context context, Cursor cursor, boolean isExpanded, ViewGroup parent) {
-				// Layout parameters for the ExpandableListView
-				AbsListView.LayoutParams lp = new AbsListView.LayoutParams(
-						ViewGroup.LayoutParams.MATCH_PARENT, 64);
-
-				TextView textView = new TextView( context);
-				textView.setLayoutParams(lp);
-				// Center the text vertically
-				textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
-				// Set the text starting position
-				textView.setPadding(36, 0, 0, 0);
-				return textView;
-				
-			}
-
-			@Override
-			protected void bindGroupView(View view, Context context, Cursor cursor, boolean isExpanded) {
-				Log.d(getClass().getSimpleName(), cursor.getString(1));
-				((TextView) view).setText(cursor.getString(1));
-			//	scaleToFit((TextView) view);
-			}
-
-			@Override
-			protected Cursor getChildrenCursor(Cursor groupCursor) {
-				return getContentResolver()
-						.query(Mnem.CONTENT_URI, Mnem.PROJECTION, null, null, null);
-				//TODO machen!
-			}
-
-			@Override
-			protected View newChildView(Context context, Cursor cursor,
-					boolean isLastChild, ViewGroup parent) {
-				TextView v = new TextView(context);
-				v.setBackgroundColor(Color.BLACK);
-				v.setTextColor(Color.LTGRAY);
-				v.setGravity(Gravity.CENTER_HORIZONTAL
-						| Gravity.CENTER_VERTICAL);
-				return v;
-			}
-
-			@Override
-			protected void bindChildView(View view, Context context,
-					Cursor cursor, boolean isLastChild) {
-				((TextView) view).setText(cursor.getString(1));
-				//scaleToFit((TextView) view);
-			}
-		};
-		setListAdapter(adapter);
-		
-		
+	// search
+	Intent intent = getIntent();
+	if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+	String query = intent.getStringExtra(SearchManager.QUERY);
+	if (query == null) query = intent.getDataString(); // touch
+	Toast.makeText(this, "Search: "+query, Toast.LENGTH_LONG).show();
 	}
-}
+
+	adapter=new CursorTreeAdapter(getContentResolver().query(Mnem.CONTENT_URI, Mnem.PROJECTION, null, null, null), this) {
+
+	@Override
+	protected View newGroupView(Context context, Cursor cursor, boolean isExpanded, ViewGroup parent) {
+	// Layout parameters for the ExpandableListView
+	AbsListView.LayoutParams lp = new AbsListView.LayoutParams(
+	ViewGroup.LayoutParams.MATCH_PARENT, 64);
+
+	TextView textView = new TextView( context);
+	textView.setLayoutParams(lp);
+	// Center the text vertically
+	textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+	// Set the text starting position
+	textView.setPadding(36, 0, 0, 0);
+	return textView;
+
+	}
+
+	@Override
+	protected void bindGroupView(View view, Context context, Cursor cursor, boolean isExpanded) {
+	Log.d(getClass().getSimpleName(), cursor.getString(1));
+	((TextView) view).setText(cursor.getString(1));
+	// scaleToFit((TextView) view);
+	}
+
+	@Override
+	protected Cursor getChildrenCursor(Cursor groupCursor) {
+	return getContentResolver()
+	.query(Mnem.CONTENT_URI, Mnem.PROJECTION, null, null, null);
+	//TODO machen!
+	}
+
+	@Override
+	protected View newChildView(Context context, Cursor cursor,
+	boolean isLastChild, ViewGroup parent) {
+	TextView v = new TextView(context);
+	v.setBackgroundColor(Color.BLACK);
+	v.setTextColor(Color.LTGRAY);
+	v.setGravity(Gravity.CENTER_HORIZONTAL
+	| Gravity.CENTER_VERTICAL);
+	return v;
+	}
+
+	@Override
+	protected void bindChildView(View view, Context context,
+	Cursor cursor, boolean isLastChild) {
+	((TextView) view).setText(cursor.getString(1));
+	//scaleToFit((TextView) view);
+	}
+	};
+	setListAdapter(adapter);
+
+
+	}
+	}
+
